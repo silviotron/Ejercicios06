ad5497104d16d86807c3916ce8fd261f5f52e853
alumno <alumno@DAW1-12>
2021-11-16 13:03:33 +0100

    aun no termine el 4

--- src/ejercicios06/Ejercicios06.java
+++ src/ejercicios06/Ejercicios06.java
@@ -6,6 +6,7 @@
 package ejercicios06;
 
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -17,49 +18,148 @@ public class Ejercicios06 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ejercicio01();
+        ejercicio04();
     }
-    public static void ejercicio01(){
+
+    public static void ejercicio01() {
         Scanner sc = new Scanner(System.in);
         double[] notas = new double[10];
         for (int i = 0; i < notas.length; i++) {
-            System.out.print("Introduce la nota: ");
-            notas[i] = sc.nextInt();
-            
+            notas[i] = -1;
         }
-        double media = calcMedia(notas);
-        double mayor = cacMayor(notas);
-        double menor = calcMenor(notas);
-                
+        for (int i = 0; i < notas.length;) {
+            System.out.printf("Por favor, inserte la %sª nota: ", i + 1);
+            if (sc.hasNextInt()) {
+                notas[i] = sc.nextInt();
+            }
+            if (notas[i] < 0 || notas[i] > 10) {
+                System.out.println("ERROR: Debe insertar una nota del 0 al 10");
+            } else {
+                i++;
+            }
+            sc.nextLine();
+        }
+
+        System.out.println("media: " + calcMedia(notas));
+        System.out.println("la nota más alta es: " + calcMayor(notas));
+        System.out.println("la nota más baja es: " + calcMenor(notas));
+
     }
-    public static double calcMedia(double[] notas){
+
+    public static double calcMedia(double[] notas) {
         double media = 0;
         for (int i = 0; i < notas.length; i++) {
             media = media + notas[i];
-            
+
         }
         return media / notas.length;
-        
+
     }
-    public static double calcMayor(double[] notas){
+
+    public static double calcMayor(double[] notas) {
         double mayor = 0;
         for (int i = 0; i < notas.length; i++) {
-            if(mayor < notas[i]){
+            if (mayor < notas[i]) {
                 mayor = notas[i];
             }
-            
-        }        
+
+        }
         return mayor;
     }
-    public static double calcmenor(double[] notas){
+
+    public static double calcMenor(double[] notas) {
         double menor = 10;
         for (int i = 0; i < notas.length; i++) {
-            if(menor > notas[i]){
+            if (menor > notas[i]) {
                 menor = notas[i];
             }
-            
-        }        
-        return menor;        
+
+        }
+        return menor;
+    }
+
+    public static void ejercicio02() {
+        int[] numeros = {2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010};
+        printIntArray(numeros);
+        int[] invertidos = new int[numeros.length];
+        int j = numeros.length - 1;
+        for (int i = 0; i < invertidos.length; i++) {
+            invertidos[i] = numeros[j];
+            j--;
+
+        }
+        printIntArray(invertidos);
+
+    }
+
+    public static void printIntArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("[" + array[i] + "]");
+
+        }
+
     }
     
+    public static void ejercicio03(){
+        int swap;
+        int[] numeros = { 16, 3, 2, 13, 5, 10, 11, 8, 9, 6, 7, 12, 4, 15, 14, 1};
+        for (int i = 0; i < numeros.length - 1; i++) {
+            if(numeros[i] > numeros[i + 1]){
+                swap = numeros[i];
+                numeros[i] = numeros[i +1];
+                numeros[i + 1] = swap;
+                i=-1;
+            }
+            
+        }
+        printIntArray(numeros);
+        
+        
+    }
+    
+    public static void ejercicio03v2(){
+        int swap;
+        int[] numeros = { 16, 3, 2, 13, 5, 10, 11, 8, 9, 6, 7, 12, 4, 15, 14, 1};
+        for (int i = 0; i < numeros.length - 1; i++) {
+            for (int j = i + 1; j < numeros.length; j++) {
+                if(numeros[i] > numeros[j]){
+                swap = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = swap;
+                }
+                
+            }
+
+            
+        }
+        printIntArray(numeros);
+        
+        
+    }
+    
+    public static void ejercicio04(){
+        String[][] nombreCompleto = new String[13][3]; 
+        String[] nombre = {"Carlos", "Miguel", "Iván", "Benjamín", "Francisco", "Erik", "Alexis Jose", "Marcos", "Cristopher", "Mauricio", "Jose Simon", "Nuria Maria", "Silvio", "Gonçalo Rafael"};
+        String[] apellido1 = {"Alvarez", "Candeira", "Casas", "Dominguez", "Fernandez", "Ferreira", "Giraldez", "Gonzalez", "Juncal", "Montes", "Sanchez", "da Silva", "Suarez", "Novas", "rodrigues"};
+        String[] apellido2 = {"Sanchez", "Carrera", "Cerqueira", "Fernandez", "Araujo", "Oset", "Groba", "Pereira", "Abeledo", "Iglesias", "Gonzalez", "Vilas", "martínez", "bento"};    
+        for(int i = 0; i < nombreCompleto.length; i++){ 
+            nombreCompleto[i][0] = nombre[random(nombre.length)];
+            nombreCompleto[i][1] = apellido1[random(apellido1.length)];
+            nombreCompleto[i][2] = apellido2[random(apellido2.length)];
+        }
+        printNombres(nombreCompleto);
+    }
+    
+    public static void printNombres(String[][] nombres){
+        for(int i = 0; i < nombres.length; i++){
+            System.out.printf("%s %s, %s\n", nombres[i][1], nombres[i][2], nombres[i][0]);
+            
+        }
+    }
+    
+    public static int random(int max){
+        Random generador = new Random();
+        return generador.nextInt(max);
+    }
+
 }
